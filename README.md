f4d7d4caeed728192e485a86d821057a92881013
Pushp Abrol <pushp.abrol@gmail.com>
2024-01-11 14:22:10 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# README for Key Generation Script
+
+## Overview
+
+This script is a Node.js application designed for generating cryptographic keys using various algorithms and configurations. It leverages `node-jose` and `jose` libraries for cryptographic operations and `inquirer` for interactive command-line prompts.
+
+## Features
+
+- **Multiple Key Types**: Supports RSA, Elliptic Curve (EC), Octet Key Pair (OKP), and Octet Sequence (oct) key types.
+- **Variety of Algorithms**: Offers a range of algorithms for both signing (sig) and encryption (enc) purposes.
+- **Customizable Key Sizes and Curves**: Allows selection of key sizes for RSA and oct keys, and curves for EC and OKP keys.
+- **Output Formats**: Generates keys in both JSON Web Key Set (JWKS) and PEM formats.
+
+## Prerequisites
+
+- Node.js
+- npm (Node Package Manager)
+
+## Installation
+
+1. Clone the repository or download the script file.
+2. Navigate to the script's directory.
+3. Run `npm install` to install required dependencies.
+
+## Usage
+
+To use the script, follow these steps:
+
+1. Run the script using `node [script-name.js]`.
+2. Follow the interactive prompts to choose the key type, purpose (signing or encryption), algorithm, and key size/curve.
+3. The script will output the generated key in JWKS and PEM formats.
+
+### Key Configuration
+
+- **RSA**: Supports key sizes of 2048, 4096, and 8192 bits.
+- **EC**: Supports `P-256`, `P-384`, `P-521`, and `secp256k1` curves.
+- **OKP**: Supports `Ed25519`, `Ed448`, `X25519`, and `X448` curves.
+- **oct**: Supports various key sizes and algorithms, including HMAC and AES.
+
+### Output
+
+- Displays the generated keys in both JWKS and PEM formats.
+- For asymmetric keys (RSA, EC), it provides both private and public keys.
+- For symmetric keys (oct), it generates a shared key.
+
+## Error Handling
+
+The script includes error handling to catch and display any issues encountered during the key generation process.
+
+## Note
+
+This script is intended for educational and development purposes. Ensure to follow best practices for key management and security in production environments. 
+
+---
+
+For further customization or issues, refer to the documentation of the `node-jose` and `jose` libraries.
---
+++ index.js
@@ -0,0 +1,206 @@
+#!/usr/bin/env node
+
+import pkg from 'node-jose';
+const { JWK } = pkg;
+
+import * as jose from 'jose'
+import inquirer from 'inquirer';
+
+const keyConfig = {
+  RSA: {
+    KeyUses: ["sig", "enc"],
+    Algorithms: {
+      sig: [
+        "RS256",
+        "RS384",
+        "RS512",
+        "PS256",
+        "PS384",
+        "PS512"
+      ],
+      enc: [
+        "RSA-OAEP",
+        "RSA-OAEP-256",
+        "RSA-OAEP-384",
+        "RSA-OAEP-512",
+        "RSA1_5"
+      ]
+    },
+    KeySizes: [2048, 4096, 8192]
+  },
+  EC: {
+    Curves: ["P-256", "P-384", "P-521", "secp256k1"],
+    KeyUses: ["sig", "enc"],
+    Algorithms: {
+      sig: [
+        "ES256",
+        "ES384",
+        "ES512",
+        "ES256K"
+      ],
+      enc: [
+        "ECDH-ES",
+        "ECDH-ES+A128KW",
+        "ECDH-ES+A192KW",
+        "ECDH-ES+A256KW"
+      ]
+    },
+    KeySizes: ["P-256", "P-384", "P-521", "secp256k1"]
+  },
+  OKP: {
+    Curves: ["Ed25519", "Ed448", "X25519", "X448"],
+    KeyUses: ["sig", "enc"],
+    Algorithms: {
+      sig: [
+        "EdDSA"
+      ],
+      enc: [
+        "ECDH-ES",
+        "ECDH-ES+A128KW",
+        "ECDH-ES+A192KW",
+        "ECDH-ES+A256KW"
+      ]
+    },
+    KeySizes: [ {"sig" : ["Ed25519","Ed448"]}, { "enc": ["P-256", "P-384", "P-521", "X25519", "X448"]}]
+  },
+  oct: {
+    KeyUses: ["enc", "sig"],
+    Algorithms: {
+      sig: [
+        "HS256",
+        "HS384",
+        "HS512"
+      ],
+      enc: [
+        "A128GCM",
+        "A192GCM",
+        "A256GCM",
+        "A128CBC-HS256",
+        "A192CBC-HS384",
+        "A256CBC-HS512",
+        "A128KW",
+        "A192KW",
+        "A256KW",
+        "dir",
+        "A128GCMKW", // Added "A128GCMKW" algorithm
+        "A192GCMKW",
+        "A256GCMKW",
+        "PBES2-HS256+A128KW",
+        "PBES2-HS384+A192KW",
+        "PBES2-HS512+A256KW"
+      ]
+    },
+    KeySizes: [
+      { "HS256": ["256"] },
+      { "HS384": ["384"] },
+      { "HS512": ["512"] },
+      { "A128GCM": ["128"] },
+      { "A192GCM": ["192"] },
+      { "A256GCM": ["256"] },
+      { "A128CBC-HS256": ["256"] },
+      { "A192CBC-HS384": ["384"] },
+      { "A256CBC-HS512": ["512"] },
+      { "A128KW": ["128"] },
+      { "A192KW": ["192"] },
+      { "A256KW": ["256"] },
+      { "dir": ["128", "192", "256"] },
+      { "A128GCMKW": ["128"] },
+      { "A192GCMKW": ["192"] },
+      { "A256GCMKW": ["256"] },
+      {"PBES2-HS256+A128KW" : ["256"]},
+      {"PBES2-HS384+A192KW" : ["384"]},
+      {"PBES2-HS512+A256KW" : ["512"]},
+    ]
+
+  }
+};
+
+
+
+async function generateKey() {
+  const questions = [
+    {
+      type: 'list',
+      name: 'keyType',
+      message: 'Choose the type of key to generate:',
+      choices: Object.keys(keyConfig),
+    },
+    {
+      type: 'list',
+      name: 'keyUse',
+      message: 'Choose the purpose of the key (sig or enc):',
+      choices: (answers) => keyConfig[answers.keyType].KeyUses,
+    },
+    {
+      type: 'list',
+      name: 'algorithm',
+      message: 'Choose an algorithm:',
+      choices: (answers) => keyConfig[answers.keyType].Algorithms[answers.keyUse],
+    },
+    {
+      type: 'list',
+      name: 'keySize',
+      message: 'Enter the key size or EC Curve for keyType - EC:',
+      choices: (answers) => {
+        if (answers.keyType === "oct") {
+          return keyConfig[answers.keyType].KeySizes.find((item) => answers.algorithm in item)[answers.algorithm];
+        }
+        else if(answers.keyType === "OKP"){
+          return keyConfig[answers.keyType].KeySizes.find((item) => answers.keyUse in item)[answers.keyUse];
+        }
+        else return keyConfig[answers.keyType].KeySizes;
+      }
+    },
+  ];
+
+  try {
+    const answers = await inquirer.prompt(questions);
+    console.log(answers);
+    const { keyType, keyUse, algorithm, keySize } = answers;
+    
+    const asymmetric = keyType === "RSA" || keyType === "EC"
+
+    if (keyType === "OKP") {
+
+      const { publicKey, privateKey } = await jose.generateKeyPair(algorithm, {crv: keySize, exportable: true });
+      console.log('Generated Public Key (JWKS):');
+      const jwk = await jose.exportJWK(publicKey);
+      jwk.use = keyUse;
+      console.log(jwk);
+      console.log('\nGenerated Public Key (PEM): ');
+      console.log(await jose.exportSPKI(publicKey));
+      console.log('Generated Key (JWKS):');
+      const jwkPrivateKey = await jose.exportJWK(privateKey);
+      jwkPrivateKey.use = keyUse;
+      console.log(jwkPrivateKey);
+      console.log('Generated Key (PEM):');
+      console.log(await jose.exportPKCS8(privateKey))
+    }
+    else {
+
+      const keystore = JWK.createKeyStore();
+      const key = await keystore.generate(keyType, keySize, { use: keyUse, alg: algorithm });
+
+      const jwksKey = key.toJSON(true);
+      const jwksKeyPEM = key.toPEM(true);
+      console.log(asymmetric ? 'Generated Key (JWKS):' : 'Shared Key (JWKS):');
+      console.log(jwksKey);
+      console.log(jwksKeyPEM);
+      if (asymmetric) {
+        const pemKey = await key.toPEM(true);
+        console.log(asymmetric ? '\nGenerated Key (PEM): ' : '\nShared Key (PEM): ');
+        console.log(pemKey);
+        console.log('Generated Public Key (JWKS):');
+        console.log(key.toJSON());
+        console.log('Generated Public Key (PEM):');
+        console.log(key.toPEM());
+      }
+    }
+
+  } catch (error) {
+    console.log(error);
+    console.error('Error generating key:', error.message);
+  }
+}
+
+generateKey();
